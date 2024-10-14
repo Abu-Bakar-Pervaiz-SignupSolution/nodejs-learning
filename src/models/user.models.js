56f623893f84a21f11cc565b8cf4d0589303836a
abu.bakar <abu.baker@signupsolution.com>
2024-10-14 12:34:41 +0500

    pushing after adding access and refresh token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -59,4 +59,33 @@ userSchema.methods.isPasswordCorrect = async function(password) {
   return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      name: this.name,
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      name: this.name
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
 export const User = mongoose.model("User", userSchema)
